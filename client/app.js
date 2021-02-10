743a804765e85741840d2227a717149f5a93d2b4
kamil.mrowka <kamil.mrowka@outlook.com.au>
2021-02-10 12:43:36 +0100

    add addmessage section

--- client/app.js
+++ client/app.js
@@ -1,27 +1,47 @@
 
 const loginForm = document.getElementById('welcome-form');
+const addMessageForm = document.getElementById('add-messages-form');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
-const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
+/* SET UP LOGINFORM */
+    var userName;
 
-var userName;
+    const welcomFormListener = () => (loginForm.onsubmit = () => {
+        signIn(userNameInput.value);
+        return false;
+    });
 
-const logListener = () => (loginForm.onsubmit = () => {
-    signIn(userNameInput.value);
-    return false;
-});
+    welcomFormListener();
 
-logListener();
-
-const signIn = name => {
-    if(name == ''){
-        alert('Login cannot be empty.')
+    const signIn = name => {
+        if(name == ''){
+            alert('Login cannot be empty.')
+        }
+        userName = name;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
     }
-    userName = name;
-    loginForm.classList.remove('show');
-    messagesSection.classList.add('show');
-}
  
+/* SET UP ADDMESSAGEFORM */
+
+    const messageFormListener = (params) => (addMessageForm.onsubmit = () => {
+        sendMessage(messageContentInput.value);
+        return false;
+    });
+
+    messageFormListener();
+
+    const sendMessage = (text) => {
+        if(text == ''){
+            alert('Text field is empty.')
+        }
+        addMessage(userName, text);
+    }
+
+    const addMessage = (userName, text) => {
+        console.log(userName, text);
+        messageContentInput.value = '';
+    }
